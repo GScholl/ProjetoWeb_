8b21a14621d09f8ad4d34d8457eb1fb4076ab0fd
Guilherme Scholl <108899269+GScholl@users.noreply.github.com>
2023-09-08 08:25:10 -0300

    Incremento de conteudo

--- js/script.js
+++ js/script.js
@@ -59,13 +59,13 @@ $(".agenda-dia").slick({
   infinite: false,
   speed: 300,
   slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         infinite: true,
         dots: true,
       },
